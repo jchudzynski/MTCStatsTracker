164ea9f3c51ac67a15fdf69bcb2560e7270132ba
Janusz Chudzynski <j.chudzynski@techsmith.com>
2020-05-10 12:05:10 -0500

    Working on maps - world one is broken

--- src/assets/scripts/vectorMaps/index.js
+++ src/assets/scripts/vectorMaps/index.js
@@ -2,18 +2,19 @@ import * as $ from 'jquery';
 import 'jvectormap';
 import 'jvectormap/jquery-jvectormap.css';
 import './jquery-jvectormap-world-mill.js';
+import './jquery-jvectormap-us-aea.js';
 import { debounce } from 'lodash';
 
 export default (function () {
   const vectorMapInit = () => {
     if ($('#world-map-marker').length > 0) {
       // This is a hack, as the .empty() did not do the work
-      $('#vmap').remove();
-
+      $('#wmap').remove();
+      $('#usamap').remove();
       // we recreate (after removing it) the container div, to reset all the data of the map
       $('#world-map-marker').append(`
         <div
-          id="vmap"
+          id="wmap"
           style="
             height: 490px;
             position: relative;
@@ -23,12 +24,30 @@ export default (function () {
         >
         </div>
       `);
-      let values = { 'AR':100,'BO':100, 'CL':100, 'CO':100,'CR':100,
+
+      $('#usa-map-marker').append(`
+        <div
+          id="usamap"
+          style="
+            height: 490px;
+            position: relative;
+            overflow: hidden;
+            background-color: transparent;
+          "
+        >
+        </div>
+      `);
+
+      let countries = { 'AR':100,'BO':100, 'CL':100, 'CO':100,'CR':100,
                   'CU':100, 'DO':100, 'EC':100, 'SV':100,'GT':100,'HN':100, 'MX':100,
                   'PA':100, 'PR':100,'PY':100,'PE':100,'ES':100,'UY':100,'VE':100, 'US':100}
 
+      let states = {'US-FL':100,'US-AZ':100,'US-CO':100,'US-CT':100,'US-HI':100,
+                    'US-IA':100,'US-ID':100,'US-IN':100,'US-NH':100,'US-NY':100,'US-MS':100,
+                  'US-NC':100,'US-DC':100,'US-DE':100,'US-DE':100,'US-ND':100,'US-MA':100,
+                'US-NJ':100,'US-TN':100,'US-MD':100 }
 
-      $('#vmap').vectorMap({
+      var generalConfig = {
         map: 'world_mill',
         backgroundColor: '#fff',
         borderColor: '#fff',
@@ -53,7 +72,7 @@ export default (function () {
         },
         series: {
           regions: [{
-            values: values,
+            values: countries,
             scale: ['#03a9f3', '#02a7f1'],
             normalizeFunction: 'polynomial',
           }],
@@ -66,7 +85,17 @@ export default (function () {
         selectedRegions: [],
         enableZoom: false,
         hoverColor: '#fff',
-      });
+      }
+
+
+      let worldConfig = Object.assign({}, generalConfig);
+      let usaConfig = Object.assign({}, generalConfig);
+      usaConfig.map = 'us_aea'
+      usaConfig.series.regions[0].values = states
+
+      $('#wmap').vectorMap(generalConfig);
+      // $('#usamap').vectorMap(usaConfig);
+
     }
   };
 
