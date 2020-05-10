9c91f681b17c67490439e91706d823ba978df742
Janusz Chudzynski <j.chudzynski@techsmith.com>
2020-05-10 11:11:50 -0500

    Basic structure of the website is ready, working on maps now

--- src/assets/scripts/vectorMaps/index.js
+++ src/assets/scripts/vectorMaps/index.js
@@ -23,6 +23,10 @@ export default (function () {
         >
         </div>
       `);
+      let values = { 'AR':100,'BO':100, 'CL':100, 'CO':100,'CR':100,
+                  'CU':100, 'DO':100, 'EC':100, 'SV':100,'GT':100,'HN':100, 'MX':100,
+                  'PA':100, 'PR':100,'PY':100,'PE':100,'ES':100,'UY':100,'VE':100, 'US':100}
+
 
       $('#vmap').vectorMap({
         map: 'world_mill',
@@ -47,32 +51,9 @@ export default (function () {
             'stroke-opacity': 0.4,
           },
         },
-
-        markers : [{
-          latLng : [21.00, 78.00],
-          name : 'INDIA : 350',
-        }, {
-          latLng : [-33.00, 151.00],
-          name : 'Australia : 250',
-        }, {
-          latLng : [36.77, -119.41],
-          name : 'USA : 250',
-        }, {
-          latLng : [55.37, -3.41],
-          name : 'UK   : 250',
-        }, {
-          latLng : [25.20, 55.27],
-          name : 'UAE : 250',
-        }],
         series: {
           regions: [{
-            values: {
-              'US': 298,
-              'SA': 200,
-              'AU': 760,
-              'IN': 200,
-              'GB': 120,
-            },
+            values: values,
             scale: ['#03a9f3', '#02a7f1'],
             normalizeFunction: 'polynomial',
           }],
