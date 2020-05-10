389a018a594d74ef6e3f0c40406c427b1ba5f8d6
Janusz Chudzynski <j.chudzynski@techsmith.com>
2020-05-10 12:37:40 -0500

    V1 ready

--- src/assets/scripts/vectorMaps/index.js
+++ src/assets/scripts/vectorMaps/index.js
@@ -9,12 +9,12 @@ export default (function () {
   const vectorMapInit = () => {
     if ($('#world-map-marker').length > 0) {
       // This is a hack, as the .empty() did not do the work
-      $('#wmap').remove();
+      $('#vmap').remove();
       $('#usamap').remove();
       // we recreate (after removing it) the container div, to reset all the data of the map
       $('#world-map-marker').append(`
         <div
-          id="wmap"
+          id="vmap"
           style="
             height: 490px;
             position: relative;
@@ -42,12 +42,13 @@ export default (function () {
                   'CU':100, 'DO':100, 'EC':100, 'SV':100,'GT':100,'HN':100, 'MX':100,
                   'PA':100, 'PR':100,'PY':100,'PE':100,'ES':100,'UY':100,'VE':100, 'US':100}
 
+
       let states = {'US-FL':100,'US-AZ':100,'US-CO':100,'US-CT':100,'US-HI':100,
                     'US-IA':100,'US-ID':100,'US-IN':100,'US-NH':100,'US-NY':100,'US-MS':100,
                   'US-NC':100,'US-DC':100,'US-DE':100,'US-DE':100,'US-ND':100,'US-MA':100,
                 'US-NJ':100,'US-TN':100,'US-MD':100 }
 
-      var generalConfig = {
+      var worldConfig = {
         map: 'world_mill',
         backgroundColor: '#fff',
         borderColor: '#fff',
@@ -85,16 +86,58 @@ export default (function () {
         selectedRegions: [],
         enableZoom: false,
         hoverColor: '#fff',
-      }
+      };
+
+      var usaConfig = {
+        map: 'us_aea',
+        backgroundColor: '#fff',
+        borderColor: '#fff',
+        borderOpacity: 0.25,
+        borderWidth: 0,
+        color: '#e6e6e6',
+        regionStyle : {
+          initial : {
+            fill : '#e4ecef',
+          },
+        },
+
+        markerStyle: {
+          initial: {
+            r: 7,
+            'fill': '#fff',
+            'fill-opacity':1,
+            'stroke': '#000',
+            'stroke-width' : 2,
+            'stroke-opacity': 0.4,
+          },
+        },
+        series: {
+          regions: [{
+            values: states,
+            scale: ['#03a9f3', '#02a7f1'],
+            normalizeFunction: 'polynomial',
+          }],
+        },
+        hoverOpacity: null,
+        normalizeFunction: 'linear',
+        zoomOnScroll: false,
+        scaleColors: ['#b6d6ff', '#005ace'],
+        selectedColor: '#c9dfaf',
+        selectedRegions: [],
+        enableZoom: false,
+        hoverColor: '#fff',
+      };
 
 
-      let worldConfig = Object.assign({}, generalConfig);
-      let usaConfig = Object.assign({}, generalConfig);
-      usaConfig.map = 'us_aea'
-      usaConfig.series.regions[0].values = states
+      // let worldConfig = Object.assign({}, generalConfig);
+      // let usaConfig = Object.assign({}, generalConfig);
+      // usaConfig.map = 'us_aea'
+      // usaConfig.series.regions[0].values = states
+      // worldConfig.series.regions[0].values = countries
 
-      $('#wmap').vectorMap(generalConfig);
-      // $('#usamap').vectorMap(usaConfig);
+
+      $('#vmap').vectorMap(worldConfig);
+      $('#usamap').vectorMap(usaConfig);
 
     }
   };
